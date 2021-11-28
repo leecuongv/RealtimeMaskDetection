f226cbb4092ae0f6d0010d7fc5d29623f9e49dc7
Lee Cường <vancuongle65@gmail.com>
2021-11-28 19:54:49 +0700

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import the necessary packages
+#In[0] import, setting
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
@@ -10,119 +10,110 @@ import cv2
 import os
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
-	# grab the dimensions of the frame and then construct a blob
-	# from it
+    # Lấy kích thước của khung và tạo mảng các điểm để nhận diện
 	(h, w) = frame.shape[:2]
 	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
 		(104.0, 177.0, 123.0))
 
 	# pass the blob through the network and obtain the face detections
+ 
 	faceNet.setInput(blob)
 	detections = faceNet.forward()
 	print(detections.shape)
 
-	# initialize our list of faces, their corresponding locations,
-	# and the list of predictions from our face mask network
+	#Khởi tạo danh sách các khuôn mặt, vị trí tương ứng của các điểm trên khuôn mặt, và danh sách các dự đoán
 	faces = []
 	locs = []
 	preds = []
-
+	#frame: Khung để nhận dạng khuôn mặt
+	#faceNet: Model để phát hiện vị trí của khuôn mặt trong ảnh
+	#maskNet: Model để phân loại mặt có đeo khẩu trang
 	# loop over the detections
 	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the detection
+		# trích xuất độ tin cậy(xác suất) được liên kết với phát hiện
 		confidence = detections[0, 0, i, 2]
 
 		# filter out weak detections by ensuring the confidence is
 		# greater than the minimum confidence
+		# lọc ra các phát hiện yếu để đảm bảo độ tin cậy lớn hơn độ tin cậy tối thiểu
 		if confidence > 0.5:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object
+			# tính toạn độ(x, y) của box để giới hạn cho mặt của đối tượng
 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 			(startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the bounding boxes fall within the dimensions of
-			# the frame
+			# đảm bảo các hộp giới hạn nằm trong kích thước của khung
 			(startX, startY) = (max(0, startX), max(0, startY))
 			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-			# extract the face ROI, convert it from BGR to RGB channel
-			# ordering, resize it to 224x224, and preprocess it
+			# trích xuất vùng mà chúng ta quan tâm(ROI) của khuôn mặt
+			# Chuyển đối từ BGR sang RGB, odering, chuẩn hóa kích thước và tiến hành tiền xử lý
 			face = frame[startY:endY, startX:endX]
 			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
 			face = cv2.resize(face, (224, 224))
 			face = img_to_array(face)
 			face = preprocess_input(face)
 
-			# add the face and bounding boxes to their respective
-			# lists
+			# thêm các hộp khuôn mặt và viền tương ứng của chúng trong danh sách
 			faces.append(face)
 			locs.append((startX, startY, endX, endY))
 
-	# only make a predictions if at least one face was detected
+	# Chỉ đưa ra dự đoán nếu phát hiện ra ít nhất một khuôn mặt
 	if len(faces) > 0:
-		# for faster inference we'll make batch predictions on *all*
-		# faces at the same time rather than one-by-one predictions
-		# in the above `for` loop
+		# để tăng tốc độ nhận diện thì chúng ta sẽ đưa ra dự đoán hàng loạt trên tất cả các khuôn mặt cùng một lúc 
+  		# thay vì nhận diện từng khuôn mặt
 		faces = np.array(faces, dtype="float32")
 		preds = maskNet.predict(faces, batch_size=32)
 
-	# return a 2-tuple of the face locations and their corresponding
-	# locations
+	# trả về bộ vị trí của các khuôn mặt
 	return (locs, preds)
-
-# load our serialized face detector model from disk
+# In[1]: load model
 prototxtPath = r"deploy.prototxt"
 weightsPath = r"res10_300x300_ssd_iter_140000.caffemodel"
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# load the face mask detector model from disk
+# load facemask model
 maskNet = load_model("mask_detector.model")
 
-# initialize the video stream
+# In[2] chuẩn bị stream video
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 
-# loop over the frames from the video stream
+# In[3]: xử lý video stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
+	# lấy từng khung hình (frame) từ video 
+ 	# và thay đổi kích thước của chúng để có chiều rộng tối đa là 400pixel
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
 
-	# detect faces in the frame and determine if they are wearing a
-	# face mask or not
+	# phát hện các khuôn mặt có trong khung hình và xác định liệu họ có đeo mặt nạ hay không 
 	(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 
-	# loop over the detected face locations and their corresponding
-	# locations
+	# sử dụng vòng lặp để phát hiện khuôn mặt và vị trí của chúng trên khung hình
 	for (box, pred) in zip(locs, preds):
-		# unpack the bounding box and predictions
 		(startX, startY, endX, endY) = box
 		(mask, withoutMask) = pred
 
-		# determine the class label and color we'll use to draw
 		# the bounding box and text
+		# xác định các class labe và màu mà chúng ta sẽ dùng để box giới hạn và văn bản hiển thị
 		label = "Mask" if mask > withoutMask else "No Mask"
 		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-		# include the probability in the label
+  
+		# hiển thị xác suất lên khung hình
 		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-		# display the label and bounding box rectangle on the output
-		# frame
+		# hiển thị xác suất nhận diện và bõ giới hạn
 		cv2.putText(frame, label, (startX, startY - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
 	# show the output frame
+	# xuất khung hình
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
+	# nhấn q để thoát chương trình
 	if key == ord("q"):
 		break
 
-# do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
